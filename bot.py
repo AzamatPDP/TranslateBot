c09b99625fe31c7de958d24d30caacc8751fe6fb
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-05-16 14:11:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,29 @@ dp = Dispatcher(bot)
 
 translator = Translator()
 
+
 @dp.message_handler(commands=["start", "help"])
 async def hello(message: types.Message):
-    await message.answer("Assalomu aleykum botimizga xush kelibsiz!\nMatningizni yuboring")
+    await message.answer("Assalomu aleykum botimizga xush kelibsiz!\nEnglish va Uzbek tilidagi matningizni yuboring")
+
 
 @dp.message_handler()
 async def get_data(message: types.Message):
-    translation = translator.translate(message.text, dest='en')
-    translated_text = translation.text
-    await message.reply(translated_text)
+    # Foydalanuvchining matnini aniqlash
+    user_language = translator.detect(message.text).lang
+
+    # Agar foydalanuvchi ingliz tilida yozgan bo'lsa, o'zbek tiliga tarjima qilish
+    if user_language == 'en':
+        translation = translator.translate(message.text, dest='uz')
+        await message.reply(f"Uzbekcha: {translation.text}")
+    # Aks holda, ingliz tiliga tarjima qilish
+    else:
+        translation = translator.translate(message.text, dest='en')
+        await message.reply(f"Inglizcha: {translation.text}")
+
+    # Tarjima qilingan matnni yuborish
+    # await message.reply(f"Tarjima: {translation.text}")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
